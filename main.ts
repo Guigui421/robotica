cf8241844df10ccf9b34f56488d40eb5d8ad8ebc
Guigui421 <156448624+Guigui421@users.noreply.github.com>
2024-01-13 15:53:26 +0100

    Actualizar main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -5,19 +5,27 @@ namespace SpriteKind {
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.swamp.swampTile3, function (sprite, location) {
     game.gameOver(false)
-    music.play(music.createSoundEffect(WaveShape.Triangle, 300, 200, 154, 0, 75, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
+    music.play(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles10, function (sprite, location) {
+    info.startCountdown(30)
+    game.splash("Tens 30 segons")
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.rock0, function (sprite, location) {
+    info.startCountdown(15)
+    game.splash("Tens 15 segons")
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tilePath3, function (sprite, location) {
     game.gameOver(true)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileDarkGrass2, function (sprite, location) {
+    game.splash("Per aquí no és")
 })
 controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
     game.splash("Hola payo")
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`Copa`, function (sprite, location) {
     game.gameOver(true)
-})
-scene.onOverlapTile(SpriteKind.Player, sprites.castle.saplingOak, function (sprite, location) {
-	
 })
 controller.B.onEvent(ControllerButtonEvent.Repeated, function () {
     music.play(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
@@ -75,15 +83,54 @@ let Cofre_2 = sprites.create(img`
     b c e e e e e e e e e e e e c b 
     b b b b b b b b b b b b b b b b 
     . b b . . . . . . . . . . b b . 
-    `, SpriteKind.Player)
+    `, SpriteKind.Cofre)
+let Cofre_3 = sprites.create(img`
+    . . b b b b b b b b b b b b . . 
+    . b e 4 4 4 4 4 4 4 4 4 4 e b . 
+    b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+    b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+    b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+    b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+    b e e e e e e e e e e e e e e b 
+    b e e e e e e e e e e e e e e b 
+    b b b b b b b d d b b b b b b b 
+    c b b b b b b c c b b b b b b c 
+    c c c c c c b c c b c c c c c c 
+    b e e e e e c b b c e e e e e b 
+    b e e e e e e e e e e e e e e b 
+    b c e e e e e e e e e e e e c b 
+    b b b b b b b b b b b b b b b b 
+    . b b . . . . . . . . . . b b . 
+    `, SpriteKind.Cofre)
+let segons = sprites.create(img`
+    . . . . c c c b b b b b . . . . 
+    . . c c b 4 4 4 4 4 4 b b b . . 
+    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+    8 7 2 e e e e e e e e e e 2 7 8 
+    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+    e b e 8 8 c c 8 8 c c c 8 e b e 
+    e e b e c c e e e e e c e b e e 
+    . e e b b 4 4 4 4 4 4 4 4 e e . 
+    . . . c c c c c e e e e e . . . 
+    `, SpriteKind.Food)
+segons.changeScale(0.1, ScaleAnchor.Middle)
+segons.setPosition(103, 120)
 Cofre_1.setPosition(56, 120)
 Cofre_2.setPosition(152, 169)
+Cofre_3.setPosition(88, 88)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
 mySprite.setScale(0.9, ScaleAnchor.Middle)
 tiles.setCurrentTilemap(tilemap`Laberint`)
 info.startCountdown(60)
-null.setPosition(120, 8)
+mySprite.setPosition(120, 8)
 forever(function () {
     if (controller.up.isPressed()) {
         mySprite.setImage(img`
